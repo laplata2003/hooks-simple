3e27b7d87a88ff7d1baa5aed66500e4836f0cab5
Gaston Dario Napoli <gaston.napoli@gmail.com>
2020-05-10 20:23:29 -0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+
+function App() {
+
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>
+          Posts
+        </button>
+        <button onClick={() => setResource('todos')}>
+          Todos
+        </button>
+      </div>
+      <div>
+        <ResourceList resource={resource} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
